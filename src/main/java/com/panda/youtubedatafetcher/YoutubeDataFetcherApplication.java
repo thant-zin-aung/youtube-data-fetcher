44c9281e3cb7cf3173e14670de8e6ec3e90bdec3
Thant Zin Aung <tza.personal.dev@gmail.com>
2025-08-05 18:59:05 +0630

    Initial commit

---
+++ src/main/java/com/panda/youtubedatafetcher/YoutubeDataFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.panda.youtubedatafetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeDataFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeDataFetcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/panda/youtubedatafetcher/YoutubeDataFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.youtubedatafetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeDataFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
